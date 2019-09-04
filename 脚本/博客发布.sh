9f0f45ede37505f29d687580e4636a4e6b0ef896
CCSH <624089195@qq.com>
2019-09-04 15:21:49 +0800

    no message

---
+++ 脚本/博客发布.sh
@@ -0,0 +1,20 @@
+
+#!/bin/sh
+
+cd /Users/chenshenghui/Desktop/项目/blog
+
+#npm audit fix --force
+#
+#npm audit
+#
+#npm install hexo - deployer - git-- save
+
+hexo clean #清除缓存 网页正常情况下可以忽略此条命令
+
+hexo g #生成静态网页
+
+hexo d #开始部署
+
+open 'https://ccsh.github.io'
+
+
---
+++ 脚本/博客启动.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd /Users/chenshenghui/Desktop/项目/blog
+open 'http://localhost:4000'
+hexo s
+
